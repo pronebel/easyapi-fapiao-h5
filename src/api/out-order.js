9e0f425f6b5dad5961cf8ce97e2faa49953690eb
leida <zhang20084@126.com>
2020-10-12 23:06:11 +0800

    fix:多余接口删除

--- src/api/out-order.js
+++ src/api/out-order.js
@@ -8,27 +8,12 @@ import {baseUrl} from "./request";
  * @see https://www.easyai.com
  */
 export const getOutOrderList = (params, page) => {
-  params.size = page.size
-  params.page = page.page
-  params.state = 0
-  params.sort = "orderTime,desc"
-  params.accessToken = localStorage.getItem("accessToken")
+  params.size = page.size;
+  params.page = page.page;
+  params.state = 0;
+  params.sort = "orderTime,desc";
+  params.accessToken = localStorage.getItem("accessToken");
   return axios.get(`${baseUrl}/out-orders`, {
     params: params
   });
-}
-
-
-/**
- * 获取外部订单列表
- */
-export const getOutOrders = (id, params) => axios.get(`${baseUrl}/out-orders`, {
-  params: {
-    params,
-    size: this.size,
-    taxNumber: localStorage.getItem("taxNumber"),
-    username: this.username,
-    state: 0,
-    accessToken: localStorage.getItem("accessToken")
-  }
-});
+};
