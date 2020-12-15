10626f9ada6d78eb07325c95caa14b27c4e4a7a1
leida <zhang20084@126.com>
2020-12-15 22:23:54 +0800

    fix:http改成https

--- src/api/qiniu.js
+++ src/api/qiniu.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 /**
  * 获取七牛KEY
  */
-export const getQiniuKey = () => axios.get(`http://api2.easyapi.com/qiniu/key`);
+export const getQiniuKey = () => axios.get(`https://api2.easyapi.com/qiniu/key`);
 
 
 /**
  * 获取七牛空间TOKEN
  */
-export const getQiniuToken = () => axios.get(`http://api2.easyapi.com/qiniu/upToken`);
+export const getQiniuToken = () => axios.get(`https://api2.easyapi.com/qiniu/upToken`);
